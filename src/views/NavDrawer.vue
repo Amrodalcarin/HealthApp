e31c013b6e0c345b4e26f471d041aad9fd19b161
Mauricio <maurimanuel92@gmail.com>
2020-04-28 19:10:53 +0100

    Adding some structure and creating an example test

---
+++ src/views/NavDrawer.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :expand-on-hover="expandOnHover"
+    :mini-variant="miniVariant"
+    absolute
+    dark
+  >
+    <UserCard />
+    <v-divider></v-divider>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import UserCard from "./../components/user-card";
+export default {
+  components: {
+    UserCard
+  },
+  data: () => ({
+    expandOnHover: true,
+    miniVariant: true,
+    drawer: true
+  })
+};
+</script>
