c1649aa50ef0be89505a456cf022b9470f9589ed
Mauricio Cavalleri <maurimanuel92@gmail.com>
2020-06-06 13:39:48 +0100

    Turn api files to templates

--- edamamConfig.js
+++ edamamConfig.js
@@ -1,13 +1,7 @@
-const api_keyFood = "320f1c564ed52ec564d56995ef7089b2";
-const app_idFood = "2642ad12";
+const api_keyFood = "your api key";
+const app_idFood = "your api id";
 
-const api_keyRecipe = "c0bda0ff4fea9513a7de0f514a4e5c2c";
-const app_idRecipe = "921041f7";
+const api_keyRecipe = "your api key";
+const app_idRecipe = "your api id";
 
-
-export {
-    api_keyFood,
-    app_idFood,
-    api_keyRecipe,
-    app_idRecipe
-}
+export { api_keyFood, app_idFood, api_keyRecipe, app_idRecipe };
